827b3b70ccefdcc52bec8cdbac9f154cc725c6bb
venetay <venetay>
2018-12-11 11:33:29 +0200

    Update

--- output_plugins/mysql.py
+++ output_plugins/mysql.py
@@ -1,17 +1,3 @@
-from __future__ import print_function
-import os
-import json
-import MySQLdb
-import logging
-import time
-import datetime
-import calendar
-import geoip2.database
-import requests
-import core.output
-from core.config import CONFIG
-
-
 class Output(core.output.Output):
 
     pass
